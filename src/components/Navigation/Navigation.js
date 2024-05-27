947a87a0bcc5dd9186c8c1b7d0f2a7326f5e7d30
krisotten <kristenlotten@gmail.com>
2024-05-27 18:28:21 -0400

    Finishing styles for navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,15 +2,17 @@ import styles from './Navigation.module.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import kotten from '../../img/kotten.png';
 
 function Navigation() {
     return (
         <>
             <nav>
-                <MenuIcon />
-                <div>
-                    <SearchIcon />
-                    <ShoppingBagIcon />
+                <MenuIcon className={styles.navIcons} />
+                <img src={kotten} className={styles.kotten} />
+                <div className={styles.rightNav} >
+                    <SearchIcon className={styles.navIcons} />
+                    <ShoppingBagIcon className={styles.navIcons} />
                 </div>
             </nav>
         </>
