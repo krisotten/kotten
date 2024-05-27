5d265d41fce78c1153a9096f93214d75d2a09b4f
krisotten <kristenlotten@gmail.com>
2024-05-27 18:56:53 -0400

    Adding styles for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,14 @@
+import styles from './Footer.css';
 
 function Footer() {
-    
+    return (
+        <>
+            <footer className={styles.footer}>
+                <p>Sign up for our mailing list!</p>
+                <div className="rectangle" />
+            </footer>
+        </>
+    );
 }
 
 export default Footer;
