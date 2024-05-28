40a1a9146443943f138c6489ab4f3e3cc8a79756
krisotten <kristenlotten@gmail.com>
2024-05-28 16:55:15 -0400

    Styling Item component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Footer from './components/Footer/Footer.js';
+import Item from './components/Item/Item.js';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Item name="Swirl Crochet Mini Bag" price="$39.99" />
       <Footer />
     </>
   );
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,15 @@
-function Item() {
-    
+import styles from './Item.module.css';
+
+function Item(props) {
+    return (
+        <div className={styles.item} >
+            <div className={styles.rectangle} />
+            <div className={styles.itemText} >
+                <p className={styles.name} >{props.name}</p>
+                <p className={styles.price} >{props.price}</p>
+            </div>
+        </div>
+    );
 }
 
 export default Item;
