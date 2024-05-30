92bb3bffb7dc98ad3e75584d4cfdb732b75daa15
krisotten <kristenlotten@gmail.com>
2024-05-29 20:38:31 -0400

    Working on getting images to show up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
 import Navigation from './components/Navigation/Navigation.js';
 import Footer from './components/Footer/Footer.js';
 import ItemList from './components/ItemList/ItemList.js';
+import Item from './components/Item/Item.js';
+
+import evilEye from "./img/evil_eye.jpg";
 
 function App() {
-  const list = [{name: 'Swirl Crochet Mini Bag', price: "$39.99"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99"},
-  {name: 'Swirl Crochet Mini Bag', price: "$39.99"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99"}
+  const list = [{name: 'Swirl Crochet Mini Bag', price: "$39.99", src: evilEye}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/brown.jpg"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/strawberry.jpg"},
+  {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/mushroom.jpg"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/skull.jpg"}
   ];
 
   return (
     <>
       <Navigation />
+      {/* <Item name="Swirl" price="$39.99" src={evilEye}/> */}
       <ItemList list={list} />
       <Footer />
     </>
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,10 @@
 import styles from './Item.module.css';
+import evilEye from "./../../img/evil_eye.jpg";
 
 function Item(props) {
     return (
         <div className={styles.item} >
-            <div className={styles.rectangle} />
+            <img src={props.src} className={styles.rectangle} />
             <div className={styles.itemText} >
                 <p className={styles.name} >{props.name}</p>
                 <p className={styles.price} >{props.price}</p>
