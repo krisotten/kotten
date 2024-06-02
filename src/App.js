8c7a0448bb4226b8867bdb0b87973e3f72314f59
krisotten <kristenlotten@gmail.com>
2024-06-02 15:17:35 -0400

    Altering App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.css';
 
 import Navigation from './components/Navigation/Navigation.js';
 import Footer from './components/Footer/Footer.js';
@@ -6,17 +6,24 @@ import ItemList from './components/ItemList/ItemList.js';
 import Item from './components/Item/Item.js';
 
 import evilEye from "./img/evil_eye.jpg";
+import strawberry from "./img/strawberry.jpg";
+import brown from "./img/brown.jpg";
+import skull from "./img/skull.jpg";
+import mushroom from "./img/mushroom.jpg";
 
 function App() {
-  const list = [{name: 'Swirl Crochet Mini Bag', price: "$39.99", src: evilEye}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/brown.jpg"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/strawberry.jpg"},
-  {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/mushroom.jpg"}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: "./img/skull.jpg"}
+  const list = [{name: 'Swirl Crochet Mini Bag', price: "$39.99", src: evilEye}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: strawberry}, {name: 'Swirl Crochet Mini Bag', price: "$39.99", src: brown},
+  
   ];
 
   return (
     <>
       <Navigation />
-      {/* <Item name="Swirl" price="$39.99" src={evilEye}/> */}
-      <ItemList list={list} />
+      <div className={styles.itemList} >
+        {list.map(item => {
+            return <Item name={item.name} price={item.price} src={item.src} className={styles.item} />
+        })}
+      </div>
       <Footer />
     </>
   );
