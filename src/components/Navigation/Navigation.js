0c8b3dc46377908a9d77c398596e73dd80cb655e
krisotten <kristenlotten@gmail.com>
2024-06-04 19:06:24 -0400

    Linking logo in nav bar to home page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,12 +4,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import kotten from '../../img/kotten.png';
 
+import {Link} from 'react-router-dom';
+
 function Navigation() {
     return (
         <>
             <nav>
                 <MenuIcon className={styles.navIcons} />
-                <img src={kotten} className={styles.kotten} />
+                <Link to="/" ><img src={kotten} className={styles.kotten} /></Link>
                 <div className={styles.rightNav} >
                     <SearchIcon className={styles.navIcons} />
                     <ShoppingBagIcon className={styles.navIcons} />
