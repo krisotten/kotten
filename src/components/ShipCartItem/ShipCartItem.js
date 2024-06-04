6ace6b08dd7598564f9bc36dd0c9a3c2cdf42944
krisotten <kristenlotten@gmail.com>
2024-06-04 19:04:35 -0400

    Adding cart items to shipping page

--- src/components/ShipCartItem/ShipCartItem.js
+++ src/components/ShipCartItem/ShipCartItem.js
@@ -1,9 +1,13 @@
 import styles from './ShipCartItem.module.css';
 
-export default function ShipCartItem() {
+export default function ShipCartItem(props) {
     return (
-        <div>
-            <img src="" />
+        <div className={styles.container}>
+            <img src={props.src} className={styles.image} />
+            <div className={styles.textContainer}>
+                <p>{props.name}</p>
+                <p>{props.price}</p>
+            </div>
         </div>
     );
 }
--- src/pages/shipping/shipping.js
+++ src/pages/shipping/shipping.js
@@ -1,8 +1,17 @@
 import styles from './shipping.module.css';
 
+import { Link } from 'react-router-dom';
+
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+import ShipCartItem from '../../components/ShipCartItem/ShipCartItem.js';
+
+import evilEye from "../../img/evil_eye.jpg";
+import strawberry from "../../img/strawberry.jpg";
+
 export default function Shipping() {
+    const list = [{name: 'Evil Eye Bag', price: "$39.99", src: evilEye}, {name: 'Strawberry Bag', price: "$39.99", src: strawberry}];
+
     return (
         <div className={styles.container}>
             <div className={styles.textContainer} >
@@ -55,13 +64,44 @@ export default function Shipping() {
                 </div>
 
                 <div className={styles.buttonContainer}>
-                    <ArrowBackIcon className={styles.backButton} />
-                    <button className={styles.button}>Continue to Billing</button>
+                    <Link to="/cart" ><ArrowBackIcon className={`${styles.backButton} ${styles.hvrGrow}`} /></Link>
+                    <button className={`${styles.button} ${styles.hvrGrow}`}>Continue to Billing</button>
                 </div>
 
             </div>
 
-            <div class={styles.priceContainer} ></div>
+            <div class={styles.priceContainer} >
+                <div className={styles.cartItems}>
+                    {list.map(item => {
+                        return <ShipCartItem src={item.src} name={item.name} price={item.price} />
+                    })}
+                </div>
+
+                <hr className={styles.line}/>
+
+                <div className={styles.price}>
+                    <div className={styles.pricePortion} >
+                        <p>Subtotal</p>
+                        <p>$39.99</p>
+                    </div>
+
+                    <div className={styles.pricePortion} >
+                        <p>Shipping</p>
+                        <p>$7.99</p>
+                    </div>
+
+                    <div className={styles.pricePortion} >
+                        <p>Taxes</p>
+                        <p>$1.99</p>
+                    </div>
+
+                    <div className={styles.total} >
+                        <p>TOTAL</p>
+                        <p>$53.99</p>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     );
