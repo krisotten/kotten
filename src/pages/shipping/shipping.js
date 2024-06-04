31538b5880d490fe0c99502a925d3db80016fd99
krisotten <kristenlotten@gmail.com>
2024-06-04 14:21:28 -0400

    Styling shipping page

--- src/pages/shipping/shipping.js
+++ src/pages/shipping/shipping.js
@@ -1,23 +1,68 @@
 import styles from './shipping.module.css';
 
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 export default function Shipping() {
     return (
         <div className={styles.container}>
-            <div className={styles.textWidth} >
+            <div className={styles.textContainer} >
                 <div>
                     <div className={styles.headContainer}>
                         <p className={styles.heading}>Contact</p>
-                        <p className={styles.required}>*Required</p>
+                        <p className={styles.required}>* Required</p>
                     </div>
                     <div className={styles.rectangle}>
-                        <p className={styles.boxText}>Email*</p>
+                        <p className={styles.boxText}>Email *</p>
                     </div>
                 </div>
+
+                <div>
+                    <div className={styles.headContainer}>
+                        <p className={styles.heading}>Shipping Address</p>
+                        <p className={styles.required}>* Required</p>
+                    </div>
+
+                    <div className={styles.address}>
+                        <div className={styles.twoRect}>
+                            <div className={styles.smallRect}>
+                                <p className={styles.boxText}>First Name *</p>
+                            </div>
+                            <div className={styles.smallRect}>
+                                <p className={styles.boxText}>Last Name *</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.rectangle}>
+                            <p className={styles.boxText}>Address *</p>
+                        </div>
+
+                        <div className={styles.rectangle}>
+                            <p className={styles.boxText}>Apartment, suite, etc. (optional) *</p>
+                        </div>
+
+                        <div className={styles.threeRect}>
+                            <div className={styles.smallRect}>
+                                <p className={styles.boxText}>City *</p>
+                            </div>
+                            <div className={styles.smallRect}>
+                                <p className={styles.boxText}>State *</p>
+                            </div>
+                            <div className={styles.smallRect}>
+                                <p className={styles.boxText}>Zip *</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.buttonContainer}>
+                    <ArrowBackIcon className={styles.backButton} />
+                    <button className={styles.button}>Continue to Billing</button>
+                </div>
+
             </div>
 
-            <div class={styles.priceContainer} >
+            <div class={styles.priceContainer} ></div>
 
-            </div>
         </div>
     );
 }
