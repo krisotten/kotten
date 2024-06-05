8ba414f2b990f3ae080656e77b14f43111c3fefc
krisotten <kristenlotten@gmail.com>
2024-06-05 18:19:40 -0400

    Adding state to sign up input to keep track of email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,24 @@ import styles from './Footer.module.css';
 // Social Media Icons
 import SendIcon from '@mui/icons-material/Send';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { SocialIcon } from 'react-social-icons';
 
+import { useState } from 'react';
+
 function Footer() {
+    const [email, setEmail] = useState("");
+
     return (
         <>
             <footer>
                 <div className={styles.footer}>
                     <div className={styles.signUp}>
                         <p className={styles.footerText}>Sign up for our mailing list!</p>
-                        <div className={styles.rectangle} />
+                        <div className={styles.emailInput}>
+                            <input className={styles.rectangle} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                            <ArrowForwardIcon className={`${styles.hvrGrow}`} />
+                        </div>
                     </div>
                     <div className={styles.socials} >
                         <SendIcon className={`${styles.mail} ${styles.hvrGrow}`} fontSize='large' />
