907f8214181740145a6f7a5cf690938189e4bf02
krisotten <kristenlotten@gmail.com>
2024-06-05 20:04:57 -0400

    Adding search functionality

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,13 +1,34 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+import styles from './search.module.css';
+
 import SearchItem from '../../components/SearchItem/SearchItem.js';
+import Item from '../../components/Item/Item.js';
+
+import {ItemContext} from '../../context/ItemContext.js';
 
 function Search() {
     const location = useLocation();
     const search = location.state;
 
+    const list = useContext(ItemContext);
+    
+    const matches = list.filter(item => item.name.toLowerCase().includes(search));
+
     return (
-        <SearchItem />
+        <>
+            <p className={styles.header}>Results for "{search}"</p>
+            <div className={styles.itemList} >
+                {matches.map(item => {
+                    return (<Link key={list.indexOf(item)} to={{pathname: `/${item.name.replaceAll(' ', '_').toLowerCase()}`}} state={item}> 
+                        <Item src={item.src} name={item.name} price={item.price} />
+                    </Link>)
+                })}
+            </div>
+        </>
     );
+    
 }
 
 export default Search;
